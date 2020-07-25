a881c1e8eb8f4813e115bdc7dd39b5c6cae1f1d9
thecml <chr1000@gmail.com>
2020-07-25 12:44:47 +0200

    added code to read raw files

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -3,28 +3,23 @@ import click
 import logging
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
+import numpy as np
+import pandas as pd
+import os
+import matplotlib.pyplot as plt
+import seaborn as sns
+import json
+from sklearn.model_selection import train_test_split
 
-
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
-
+def main():
+    df = pd.DataFrame()
+    for dirpath, dirnames, filenames in os.walk('./data/raw'):
+        for filename in filenames:
+            if filename.endswith('.json'): 
+                file = open(os.path.join(dirpath, filename), 'r')
+                df = pd.concat([df, pd.read_json(file)])
+    print("Finished reading csv")
+    print(len(df))
 
 if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
-
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
     main()
