53405a928ec27dfd4b1f53de085469ca5f75cb48
Michael <au543892@uni.au.dk>
2020-08-01 19:14:16 +0200

    did something with the columns

--- src/train_model.py
+++ src/train_model.py
@@ -31,9 +31,9 @@ def make_model():
 def prepare_data(df):
     cols_leftover = '7_x|8_x|7_y|8_y|'
     cols_grenade = 'Grenade|grenade|Flashbang|C4|'
-    cols_pistol = 'Deagle|FiveSeven|Clock|UspS|P250|P2000|Tec9|'
+    # cols_pistol = 'Deagle|FiveSeven|Glock|UspS|P250|P2000|Tec9|Cz75Auto'
     cols_misc = 'None|Dead|Zeus|snapshot_id'
-    all_cols = cols_leftover + cols_grenade + cols_pistol + cols_misc
+    all_cols = cols_leftover + cols_grenade + cols_misc
     df = df.drop(df.columns[df.columns.str.contains(all_cols)], axis=1)
 
     # Delete another leftover column
@@ -46,9 +46,49 @@ def prepare_data(df):
     # Sum all the health of the players
     # TODO: How do we check that the player plays on CT/T team?
     # Each player has a player_1_team_CT and player_1_team_Terrorist column
-    df['ct_health'] = df[df.columns[df.columns.str.contains('health')]].sum(axis=1)
+    numPlayers = 10
+    df['ct_health'] = sumPlayerCols(df, 'health', 'CT')
+    df['t_health'] = sumPlayerCols(df, 'health', 'Terrorist')
+    df['ct_armor'] = sumPlayerCols(df, 'armor', 'CT')
+    df['t_armor'] = sumPlayerCols(df, 'armor', 'Terrorist')
+    df['ct_money'] = sumPlayerCols(df, 'money', 'CT')
+    df['t_money'] = sumPlayerCols(df, 'money', 'Terrorist')
+    df['ct_helmets'] = sumPlayerCols(df, 'has_helmet', 'CT')
+    df['t_helmets'] = sumPlayerCols(df, 'has_helmet', 'Terrorist')
+    df['ct_defuse_kits'] = sumPlayerCols(df, 'has_defuser', 'CT')
+    df['ct_players'] = sum([df[str(f'player_{i}_team_CT')] for i in range(1, numPlayers + 1)])
+    df['t_players'] = sum([df[str(f'player_{i}_team_Terrorist')] for i in range(1, numPlayers + 1)])
 
+    # Weapons
+    weapon_list = ['Ak47', 'Aug', 'Awp', 'Bizon', 'Cz75Auto', 'Elite', 'Famas', 'G3sg1', 'GalilAr', 'Glock', 'M249', 'M4a1S', 'M4a4', 'Mac10', 'Mag7', 'Mp5sd', 'Mp7', 'Mp9', 'Negev', 'Nova', 'P90', 'R8Revolver', 'Sawedoff', 'Scar20', 'Sg553', 'Ssg08', 'Ump45', 'Xm1014', 'Deagle', 'FiveSeven', 'Glock', 'UspS', 'P250', 'P2000', 'Tec9', 'Cz75Auto']
+
+    for weapon in weapon_list:
+        df[f'ct_weapon_{weapon}'] = sumPlayerCols(df, f'weapon_1_{weapon}', 'CT') + sumPlayerCols(df, f'weapon_2_{weapon}', 'CT')
+        df[f't_weapon_{weapon}'] = sumPlayerCols(df, f'weapon_1_{weapon}', 'Terrorist') + sumPlayerCols(df, f'weapon_2_{weapon}', 'Terrorist')
+
+    # player_health_cols = [f'player_{i}_health' for i in range(1, numPlayers + 1)]
+    # player_armor_cols = [f'player_{i}_armor' for i in range(1, numPlayers + 1)]
+    # player_money_cols = [f'player_{i}_money' for i in range(1, numPlayers + 1)]
+    # player_helmet_cols = [f'player_{i}_has_helmet' for i in range(1, numPlayers + 1)]
+    # player_defuse_kits_cols = [f'player_{i}_has_defuser' for i in range(1, numPlayers + 1)]
+    # player_team_ct_cols = [f'player_{i}_team_CT' for i in range(1, numPlayers + 1)]
+    # player_team_terrorist_cols = [f'player_{i}_team_Terrorist' for i in range(1, numPlayers + 1)]
+    # player_ct_weapons_cols = [f'player_{i}_team_Terrorist' for i in range(1, numPlayers + 1)]
+    df = df.drop(df.columns[df.columns.str.contains('player_')], axis=1)
+    
+    # df = df.drop(
+    #     player_health_cols +
+    #     player_armor_cols +
+    #     player_money_cols +
+    #     player_helmet_cols +
+    #     player_defuse_kits_cols +
+    #     player_team_ct_cols +
+    #     player_team_terrorist_cols,
+    #     axis=1)
     return df
+
+def sumPlayerCols(df, name, team, numPlayers=10):
+    return sum([df[str(f'player_{i}_' + name)].where(df[str(f'player_{i}_team_' + team)] == 1.0, 0) for i in range(1, numPlayers + 1) if str(f'player_{i}_' + name) in df.columns])
 
 if __name__ == '__main__':
     main()
