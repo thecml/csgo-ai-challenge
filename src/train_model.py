41801299b6181ef2a42b8f6487cf8c6c824e8e72
thecml <chr1000@gmail.com>
2020-08-02 14:11:56 +0200

    first model

--- src/train_model.py
+++ src/train_model.py
@@ -3,18 +3,21 @@ import pandas as pd
 import src.file_reader as fr
 import src.file_writer as fw
 import src.config as cfg
-import tensorflow as tf
+from sklearn.preprocessing import StandardScaler
 from tensorflow import keras
 
 def main():
-    tf.test.is_built_with_cuda()
-    nrows = 100
-    X_train = fr.read_csv(cfg.INTERIM_DATA_DIR, 'X_train.csv', nrows=nrows)
-    X_valid = fr.read_csv(cfg.INTERIM_DATA_DIR, 'X_valid.csv', nrows=nrows)
-    y_train = fr.read_csv(cfg.INTERIM_DATA_DIR, 'y_train.csv', nrows=nrows)
-    y_valid = fr.read_csv(cfg.INTERIM_DATA_DIR, 'y_valid.csv', nrows=nrows)
+    X_train = fr.read_csv(cfg.PROCESSED_DATA_DIR, 'X_train.csv')
+    X_valid = fr.read_csv(cfg.PROCESSED_DATA_DIR, 'X_valid.csv')
+    y_train = fr.read_csv(cfg.PROCESSED_DATA_DIR, 'y_train.csv')
+    y_valid = fr.read_csv(cfg.PROCESSED_DATA_DIR, 'y_valid.csv')
+
+    X_train, X_valid = scale_data(X_train, X_valid)
 
     model = make_model()
+
+    history = model.fit(X_train, np.array(y_train), epochs=10,
+     validation_data=(X_valid, np.array(y_valid)))
 
 def make_model():
     model = keras.models.Sequential()
@@ -23,6 +26,12 @@ def make_model():
     model.add(keras.layers.Dense(1, activation="sigmoid"))
     model.compile(loss='binary_crossentropy', optimizer='sgd', metrics=['accuracy'])
     return model
+
+def scale_data(X_train, X_valid):
+    scaler = StandardScaler()
+    X_train = scaler.fit_transform(X_train)
+    X_valid = scaler.transform(X_valid)
+    return (X_train, X_valid)
 
 if __name__ == '__main__':
     main()
