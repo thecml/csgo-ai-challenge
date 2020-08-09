52175a46d109b64e45e4b83c8c4513a4460bf72b
thecml <chr1000@gmail.com>
2020-08-09 23:27:27 +0200

    more model work

--- src/train_model.py
+++ src/train_model.py
@@ -18,46 +18,46 @@ def main():
     X = fr.read_csv(cfg.PROCESSED_DATA_DIR, 'X_full.csv')
     y = fr.read_csv(cfg.PROCESSED_DATA_DIR, 'y_full.csv')
 
-    #X = prepare_data(X)
+    X = prepare_data(X)
 
     X_train_full, X_test, y_train_full, y_test = train_test_split(X, y,
-     test_size=0.1, random_state=0)
+     stratify=y, test_size=0.1, random_state=0)
     X_train, X_valid, y_train, y_valid = train_test_split(X_train_full, y_train_full,
-     test_size=0.25, random_state=0)
+     stratify=y_train_full, test_size=0.25, random_state=0)
 
-    keras_free = make_keras_model(n_layers=4)
-    keras_regularized = make_keras_model(n_layers=4, regularized=True, dropout=False)
+    keras_shallow = make_keras_model(n_layers=4)
+    keras_deep = make_keras_model(n_layers=8, regularized=False, dropout=True)
     keras_dropout = make_keras_model(n_layers=4, regularized=False, dropout=True)
 
-    tensorboard_cb_free = keras.callbacks.TensorBoard(get_run_logdir('free'))
-    tensorboard_cb_regularized = keras.callbacks.TensorBoard(get_run_logdir('regularized'))
+    tensorboard_cb_shallow = keras.callbacks.TensorBoard(get_run_logdir('shallow'))
+    tensorboard_cb_deep = keras.callbacks.TensorBoard(get_run_logdir('deep'))
     tensorboard_cb_dropout = keras.callbacks.TensorBoard(get_run_logdir('dropout'))
-    reduce_lr_cb_free = get_keras_reducelr_cb()
-    reduce_lr_cb_regularized = get_keras_reducelr_cb()
+    reduce_lr_cb_shallow = get_keras_reducelr_cb()
+    reduce_lr_cb_deep = get_keras_reducelr_cb()
     reduce_lr_cb_dropout = get_keras_reducelr_cb()
 
-    model_checkpoint_cb_free = keras.callbacks.ModelCheckpoint(get_run_modeldir('free'))
-    model_checkpoint_cb_regularized = keras.callbacks.ModelCheckpoint(get_run_modeldir('regularized'))
+    model_checkpoint_cb_shallow = keras.callbacks.ModelCheckpoint(get_run_modeldir('shallow'))
+    model_checkpoint_cb_deep = keras.callbacks.ModelCheckpoint(get_run_modeldir('deep'))
     model_checkpoint_cb_dropout = keras.callbacks.ModelCheckpoint(get_run_modeldir('dropout'))
 
-    history_free = keras_free.fit(np.array(X_train), np.array(y_train), epochs=100,
+    history_shallow = keras_shallow.fit(np.array(X_train), np.array(y_train), epochs=200,
      validation_data=(np.array(X_valid), np.array(y_valid)),
-      callbacks=[tensorboard_cb_free, model_checkpoint_cb_free, reduce_lr_cb_free])
+      callbacks=[tensorboard_cb_shallow, model_checkpoint_cb_shallow, reduce_lr_cb_shallow])
     
-    history_regularized = keras_regularized.fit(np.array(X_train), np.array(y_train), epochs=100,
+    history_deep = keras_deep.fit(np.array(X_train), np.array(y_train), epochs=250,
      validation_data=(np.array(X_valid), np.array(y_valid)),
-      callbacks=[tensorboard_cb_regularized, model_checkpoint_cb_regularized, reduce_lr_cb_regularized])
+      callbacks=[tensorboard_cb_deep, model_checkpoint_cb_deep, reduce_lr_cb_deep])
 
-    history_dropout = keras_dropout.fit(np.array(X_train), np.array(y_train), epochs=100,
+    history_dropout = keras_dropout.fit(np.array(X_train), np.array(y_train), epochs=250,
      validation_data=(np.array(X_valid), np.array(y_valid)),
       callbacks=[tensorboard_cb_dropout, model_checkpoint_cb_dropout, reduce_lr_cb_dropout])
 
-    results_free = keras_free.evaluate(X_test, y_test)
-    results_regularized = keras_regularized.evaluate(X_test, y_test)
+    results_shallow = keras_shallow.evaluate(X_test, y_test)
+    results_deep = keras_deep.evaluate(X_test, y_test)
     results_dropout = keras_dropout.evaluate(X_test, y_test)
 
-    print("Free test loss, test acc:", results_free)
-    print("Regularized test loss, test acc:", results_regularized)
+    print("Shallow test loss, test acc:", results_shallow)
+    print("Deep test loss, test acc:", results_deep)
     print("Dropout test loss, test acc:", results_dropout)
 
 def make_keras_model(n_layers, regularized=False, dropout=False):
@@ -66,14 +66,14 @@ def make_keras_model(n_layers, regularized=False, dropout=False):
     for n in range(n_layers):
         if regularized:
             model.add(keras.layers.Dense(300, kernel_initializer="he_normal",
-             kernel_regularizer=keras.regularizers.l2(0.1), use_bias=False))
+             kernel_regularizer=keras.regularizers.l1(0.01), use_bias=False))
         else:
             model.add(keras.layers.Dense(300,
              kernel_initializer="he_normal", use_bias=False))
         model.add(keras.layers.BatchNormalization())
         model.add(keras.layers.Activation("elu"))
         if dropout:
-            model.add(keras.layers.AlphaDropout(rate=0.1))
+            model.add(keras.layers.Dropout(rate=0.2))
     model.add(keras.layers.Dense(1, activation="sigmoid"))
     model.compile(loss='binary_crossentropy', optimizer='Nadam', metrics=['accuracy'])
     return model
@@ -93,18 +93,11 @@ def get_keras_reducelr_cb():
 
 def prepare_data(df):
     # Add 1m55s to round if freezetime
-    #df['round_status_time_left'] = df.apply(add_round_time, axis=1)
-    #df = df.drop(['round_status_FreezeTime', 'round_status_Normal'], axis=1)
+    df['round_status_time_left'] = df.apply(add_round_time, axis=1)
+    df = df.drop(['round_status_FreezeTime', 'round_status_Normal'], axis=1)
 
     # Clip roundtime value
-    #df['round_status_time_left'] = df['round_status_time_left'].clip(0, 175)
-
-    # Drop map columns
-    #df = df.drop(df.columns[df.columns.str.contains('map')], axis=1)
-
-    # Drop pistol columns
-    #pistol_cols = 'Cz75Auto|Elite|Glock|P250|UspS|P2000|Tec9|FiveSeven'
-    #df = df.drop(df.columns[df.columns.str.contains(pistol_cols)], axis=1)
+    df['round_status_time_left'] = df['round_status_time_left'].clip(0, 175)
 
     # Make data more Gaussian-like
     cols = ['round_status_time_left', 'ct_money', 't_money', 'ct_health',
